6414cf21f110ee959a53a5a99f4ba19aa0db14e7
Andy Kolski <andykolski@gmail.com>
2021-05-15 00:14:25 -0600

    Rewrote serial driver to be better in basically every way.
    New serial driver supports:
    - Multiple serial ports
    - Serial port configuration (Baud rate, bits per char, stop bits, parity)
    - Serial port interrupts and error handling
    - Probably more that I forgot

--- src/serial.c
+++ src/serial.c
@@ -1,64 +1,146 @@
 #include <display.h>
 #include <io.h>
 #include <irq.h>
+#include <keyboard.h>
 #include <libs.h>
+#include <stdio.h>
 #include <string.h>
 #include <system.h>
 
-#define PORT 0x3f8   /* COM1 */
-volatile bool IS_SERIAL_ENABLED = false;
- 
-int serial_received() {
-	if (!IS_SERIAL_ENABLED) {return 0;}
-	return inb(PORT + 5) & 1;
-}
- 
-char read_serial() {
-	if (!IS_SERIAL_ENABLED) {return 0;}
-	while (serial_received() == 0) {} //wait until we get something
- 
-	return (char)inb(PORT);
-}
+#define COM1 0x3F8
+#define COM2 0x2F8
+#define COM3 0x3E8
+#define COM4 0x2E8
 
-int is_transmit_empty() {
-	if (!IS_SERIAL_ENABLED) {return 1;}
-	return inb(PORT + 5) & 0x20;
-}
-
-void serial_putc(char chr) {
-	if (!IS_SERIAL_ENABLED) {return;}
-	while (is_transmit_empty() == 0) {} //wait until the buffer is empty
- 
-	outb(PORT, (uint8)chr);
-}
-
-void serial_puts(char *text) {
-	if (!IS_SERIAL_ENABLED) {return;}
-	for (uint32 i = 0; i < strlen(text); ++i) {
-		serial_putc(text[i]);
+uint16 getPortFromNumber(uint8 com) {
+	switch(com) {
+		case 1:
+			return COM1;
+		case 2:
+			return COM2;
+		case 3:
+			return COM3;
+		case 4:
+			return COM4;
+		default:
+			assertf("Invalid COM number");
+			return 0;
 	}
 }
 
+uint8 bitsPerCharFromNumber(uint8 number) {
+	switch(number) {
+		case 5:
+			return 0b00;
+		case 6:
+			return 0b01;
+		case 7:
+			return 0b10;
+		case 8:
+			return 0b11;
+		default:
+			assertf("Invalid number of bits per character");
+			return 0;
+	}
+}
+
+bool configurePort(uint8 com, uint32 baudRate, uint8 bitsPerChar, bool useTwoStopBits, bool useParity, bool evenParity, bool stickParity) {
+
+	uint16 port = getPortFromNumber(com);
+
+	uint8 bitsPerCharValue = bitsPerCharFromNumber(bitsPerChar);
+
+	uint16 divisor = 115200 / baudRate;
+
+
+	outb(port + 3, 0x00); // set DTAB to 0
+	outb(port + 1, 0x00); // Interrupt Enable Register - Disable interrupts
+
+
+	outb(port + 3, 0x80); // set DTAB to 1
+	outb(port + 0, divisor & 0xff); // Baud rate divisor least significant byte
+	outb(port + 1, divisor >> 8); // Baud rate divisor most significant byte
+
+	outb(port + 2, 0x07); // FIFO control registers - Interrupt after 1 byte, clear everything
+
+	uint8 portReg3Value = (bitsPerCharValue & 0x03) | (useTwoStopBits << 2) | (useParity << 3) | (evenParity << 4) | (stickParity << 5); // Line Control Register - bits 0-1 are bits per char, bit 2 is stop bits, 3 is parity enable, 4 is even parity, 5 is stick parity
+	outb(port + 3, portReg3Value);
+
+	outb(port + 4, 0x10); // Modem Control Register - we enable loopback to test the port
+
+	outb(port + 0, 0xaa);
+
+	if(inb(port + 0) != 0xaa) {
+	      return false;
+	}
+
+	outb(port + 3, 0x00); // set DTAB to 0
+	outb(port + 1, 0x01); // Interrupt Enable Register - Enable interrupts
+	outb(port + 3, portReg3Value); // restore original settings after using to set DTAB
+	outb(port + 4, 0x0B); // the port seems to work, we enable it
+
+	return true;
+}
+
+volatile bool portsEnabled[4] = {0};
+volatile uint8 usePort = 0;
+
 void serial_handler(struct regs *r __attribute__((__unused__))) {
-	char i = read_serial();
-	if (i == '\b') {
-		termBackspace();
-	} else {
-		// printf("c: %i ", i);
-		putc(i);
+	for (int i = 0; i < 4; ++i) {
+		if (!portsEnabled[i]) {
+			continue;
+		} else {
+			uint16 port = getPortFromNumber(i+1);
+			uint8 intStatus = inb(port + 2);
+			if ((intStatus & 0x01) == 1) { // this port has no pending interrupts to handle
+				continue;
+			} else { // the current port has an interrupt we need to handle
+				uint8 status = (intStatus & 0x06) >> 1;
+				if (status == 0) { // Modem Status Interrupt
+
+				} else if (status == 1) { // Transmitter Holding Register Empty Interrupt
+					
+				} else if (status == 2) { // Data received
+					uint8 read = inb(port + 0);
+					if (i+1 == usePort) {
+						keyboardKeyPress((char)read);
+					}
+					// printf("Serial in: %c\n", read);
+				} else if (status == 3) { // Error
+					uint8 error = inb(port + 5);
+					error = (error >> 1) % 0x0f;
+					if (error & 0x01) {
+						printf("Serial port %i had an error: Overrun Error\n", i+1);
+					} else if ((error >> 1) & 0x01) {
+						printf("Serial port %i had an error: Parity Error\n", i+1);
+					} else if ((error >> 2) & 0x01) {
+						printf("Serial port %i had an error: Framing Error\n", i+1);
+					} else if ((error >> 3) & 0x01) {
+						printf("Serial port %i had an error: Break Interrupt Error\n", i+1);
+					}
+				}
+			}
+		}
 	}
 }
 
 void init_serial() {
-	IS_SERIAL_ENABLED = true;
+	for (int i = 0; i < 4; ++i) {
+		portsEnabled[i] = configurePort(i+1, 9600, 8, 0, 0, 0, 0); // Try to configure the port at: 9600 baud, 8 bits per char, one stop bit, and no parity
+		if (portsEnabled[i] && !usePort) {
+			usePort = i+1;
+		}
+	}
+	printf("Using serial port: %i\n", usePort);
 	irq_install_handler(4, serial_handler);
-	outb(PORT + 1, 0x01);    // Enable basic interrupts
-	outb(PORT + 3, 0x80);    // Enable DLAB (set baud rate divisor)
-	outb(PORT + 0, 0x03);    // Set divisor to 3 (lo byte) 38400 baud
-	outb(PORT + 1, 0x00);    //                  (hi byte)
-	outb(PORT + 3, 0x03);    // 8 bits, no parity, one stop bit
-	outb(PORT + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
-	outb(PORT + 4, 0x0B);    // IRQs enabled, RTS/DSR set
+	irq_install_handler(3, serial_handler);
+}
 
-	serial_puts("Serial connection initialized!\r\n");
+void serial_putc(char chr) {
+	if (usePort == 0 || !portsEnabled[usePort-1]) {
+		return;
+	}
+	uint16 port = getPortFromNumber(usePort);
+	while (!(inb(port + 5) & 0x20)) {}
+	outb(port, (uint8)chr);
 }
--- src/serial.h
+++ src/serial.h
@@ -3,5 +3,5 @@
 #pragma once
 
 void init_serial();
-void serial_puts(char *text);
 void serial_putc(char chr);
+bool configurePort(uint8 com, uint32 baudRate, uint8 bitsPerChar, bool useTwoStopBits, bool useParity, bool evenParity, bool stickParity);
