bffb033aef66df1422a03b047a6a0e0799d7b6a0
Andy Kolski <andykolski@gmail.com>
2022-02-19 11:23:58 -0700

    Properly configure the FPU before the fninit instruction

--- src/bootstrap.asm
+++ src/bootstrap.asm
@@ -5,7 +5,16 @@ bootstrap_entry:
 	mov esp, stack_top
 	push ebx
 	push eax
+
+	mov eax, cr0 ; Configure and initialize the FPU before jumping to the C kernel
+	and eax, ~0b1100
+	or eax, 0b110010
+	mov cr0, eax
+	mov eax, cr4
+	or eax, 0b11000000000
+	mov cr4, eax
 	fninit
+
 	extern kmain
 	call kmain
 	jmp $
