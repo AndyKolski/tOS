64e0fa1990f3a52a677aad15da1900c3cdf8bf31
Andy Kolski <andykolski@gmail.com>
2023-11-24 14:00:26 -0700

    Some cleanup in RTC code

--- src/kernel/time/rtc.c
+++ src/kernel/time/rtc.c
@@ -1,45 +1,50 @@
-#include <io.h>
 #include <interrupts/irq.h>
+#include <io.h>
 #include <stdio.h>
 #include <system.h>
 #include <time/time.h>
 
-#define rtc_address 0x70
-#define rtc_data 0x71
+#define RTC_REGISTER_SELECT_PORT 0x70
+#define RTC_DATA_PORT            0x71
 
-#define update_in_progress_flag 0x80
+#define RTC_REGISTER_SECONDS  0x00
+#define RTC_REGISTER_MINUTES  0x02
+#define RTC_REGISTER_HOURS    0x04
+#define RTC_REGISTER_WEEKDAY  0x06
+#define RTC_REGISTER_DAY      0x07
+#define RTC_REGISTER_MONTH    0x08
+#define RTC_REGISTER_HALFYEAR 0x09
+#define RTC_REGISTER_CENTURY  0x32
+#define RTC_REGISTER_STATUS_A 0x0A
+#define RTC_REGISTER_STATUS_B 0x0B
+#define RTC_REGISTER_STATUS_C 0x0C
 
-#define seconds_register 0x00
-#define minutes_register 0x02
-#define hours_register 0x04
-#define weekday_register 0x06
-#define day_of_month_register 0x07
-#define month_register 0x08
-#define year_register 0x09
-#define century_register 0x32
-#define status_register_a 0x0A
-#define status_register_b 0x0B
+#define RTC_UPDATE_IN_PROGRESS_FLAG 0x80
 
 uint8 bcdToDecimal(uint8 bcd) {
-	assert(((bcd & 0xF0) >> 4) < 10, "invalid BCD conversion");  // More significant nibble is valid
-	assert((bcd & 0x0F) < 10, "invalid BCD conversion");         // Less significant nibble is valid
+	assert(((bcd & 0xF0) >> 4) < 10, "invalid BCD conversion"); // More significant nibble is valid
+	assert((bcd & 0x0F) < 10, "invalid BCD conversion");        // Less significant nibble is valid
 	return ((bcd & 0xF0) >> 4) * 10 + (bcd & 0x0F);
 }
 
-bool NMIsEnabled = true;
-
-bool isRTCUpdating() {
-	  outb(rtc_address, 0x0A);
-	  return (inb(rtc_data) & update_in_progress_flag);
-}
+bool disableNMIs = false;
 uint8 readCMOSRegister(uint8 reg) {
-	outb(rtc_address, (!NMIsEnabled << 7) | (reg));
-	return inb(rtc_data);
+	// PCs have a weird old hack here. When the 7th bit of the address port is 1, NMIs are disabled and aren't sent to the CPU
+	outb(RTC_REGISTER_SELECT_PORT, (disableNMIs << 7) | (reg));
+	io_wait();
+	return inb(RTC_DATA_PORT);
+}
+
+/// @brief Checks if the RTC is part-way through updating the values in the registers (and so they may not be valid)
+/// @return True if the RTC is updating, false otherwise
+bool isRTCUpdating() {
+	outb(RTC_REGISTER_SELECT_PORT, RTC_REGISTER_STATUS_A);
+	return (inb(RTC_DATA_PORT) & RTC_UPDATE_IN_PROGRESS_FLAG);
 }
 
 void RTC_handler(struct regs *r __attribute__((__unused__))) { // set to exactly 1024 Hz by default. We don't change the default
-	outb(rtc_address, 0x0C);
-	uint8 source = inb(rtc_data);
+	outb(RTC_REGISTER_SELECT_PORT, RTC_REGISTER_STATUS_C);
+	uint8 source = inb(RTC_DATA_PORT);
 
 	if (source & 0x40) { // periodic interrupt
 		RTC_Tick();
@@ -48,30 +53,24 @@ void RTC_handler(struct regs *r __attribute__((__unused__))) { // set to exactly
 	} else if (source & 0x10) { // Update-ended interrupt
 		// printf("RTC Update-ended int\n");
 	}
-
 }
 
-void RTC_install() {
-	irq_install_handler(8, RTC_handler);
-
-	outb(rtc_address, 0x8B);
-	uint8 prev = inb(rtc_data);
-	outb(rtc_address, 0x8B);
-	outb(rtc_data, prev | 0x40);
-
+HumanTime readRTC() {
+	// Wait until the RTC is not updating the time before we read it
 	while (isRTCUpdating()) {}
-	uint8 statusRegisterBValue = readCMOSRegister(status_register_b);
+
+	uint8 statusRegisterBValue = readCMOSRegister(RTC_REGISTER_STATUS_B);
 
 	bool twentyFourHourMode = statusRegisterBValue & 0x02;
 	bool binaryMode = statusRegisterBValue & 0x04;
 
-	uint8 seconds = readCMOSRegister(seconds_register);
-	uint8 minutes = readCMOSRegister(minutes_register);
-	uint8 hours = readCMOSRegister(hours_register);
-	uint8 day = readCMOSRegister(day_of_month_register);
-	uint8 month = readCMOSRegister(month_register);
-	uint8 halfYear = readCMOSRegister(year_register);
-	uint8 century = readCMOSRegister(century_register);
+	uint8 seconds = readCMOSRegister(RTC_REGISTER_SECONDS);
+	uint8 minutes = readCMOSRegister(RTC_REGISTER_MINUTES);
+	uint8 hours = readCMOSRegister(RTC_REGISTER_HOURS);
+	uint8 day = readCMOSRegister(RTC_REGISTER_DAY);
+	uint8 month = readCMOSRegister(RTC_REGISTER_MONTH);
+	uint8 halfYear = readCMOSRegister(RTC_REGISTER_HALFYEAR);
+	uint8 century = readCMOSRegister(RTC_REGISTER_CENTURY);
 	uint32 year = 0;
 
 	// printf("24 Hour mode: %i, Binary mode: %i\n", twentyFourHourMode, binaryMode);
@@ -92,6 +91,8 @@ void RTC_install() {
 
 	year = (uint32)(century * 100) + halfYear;
 
+	// printf("%i/%i/%i %i:%i:%i\n", month, day, year, hours, minutes, seconds);
+
 	HumanTime time = {0};
 
 	time.seconds = seconds;
@@ -101,10 +102,17 @@ void RTC_install() {
 	time.month = month;
 	time.year = year;
 
-	// printf("%i/%i/%i %i:%i:%i\n", month, day, year, hours, minutes, seconds);
-	setTime(getTimeFromHuman(time));
-
+	return time;
 }
 
+void RTC_install() {
+	irq_install_handler(8, RTC_handler);
 
+	// Enable interrupts from the RTC
+	outb(RTC_REGISTER_SELECT_PORT, 0x8B);
+	uint8 prev = inb(RTC_DATA_PORT);
+	outb(RTC_REGISTER_SELECT_PORT, 0x8B);
+	outb(RTC_DATA_PORT, prev | 0x40);
 
+	setTime(getTimeFromHuman(readRTC()));
+}
--- src/kernel/time/rtc.h
+++ src/kernel/time/rtc.h
@@ -1,5 +1,8 @@
 #include <system.h>
+#include <time/time.h>
 
 #pragma once
+
+HumanTime readRTC();
 
 void RTC_install();
