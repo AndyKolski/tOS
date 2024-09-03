AndyKolski/tOS

commit aa0844d07d0298eab3d774b54361b933625120a9

Added convenience function boolToString. Improved compile time assertions. Removed assertf() in favor of panic(). Improved IRQ, ISR, PIC code readability. Added debug logging to multiboot2 header parser. Moved regs structure to isrs.h

src/kernel/multibootdata.c
@@ -50,17 +50,17 @@ void parseMultibootData(uint32 bootloaderMagic, uint32 multibootPhysLocation) {
 	struct multiboot_header_tag *tag = multibootData + sizeof(struct multibootInformationStructureData);
 	struct multiboot_tag_string *stringTag;
 	while (tag->type != MULTIBOOT_TAG_TYPE_END) {
-		// printf("Found tag with type: %d, size: %d\n", tag->type, tag->size);
+		DEBUG(printf("Found tag with type: %d, size: %d\n", tag->type, tag->size););
 		switch (tag->type) {
 			case MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME:
 				stringTag = (struct multiboot_tag_string *)tag;
 				bootData.bootloaderName = stringTag->string;
-				// printf("Bootloader name: %s\n", bootData.bootloaderName);
+				DEBUG(printf("Bootloader name: %s\n", bootData.bootloaderName););
 				break;
 			case MULTIBOOT_TAG_TYPE_CMDLINE:
 				stringTag = (struct multiboot_tag_string *)tag;
 				bootData.cmdline = stringTag->string;
-				// printf("Command line: %s\n", bootData.cmdline);
+				DEBUG(printf("Command line: %s\n", bootData.cmdline););
 				break;
 
 			case MULTIBOOT_TAG_TYPE_FRAMEBUFFER:
@@ -73,7 +73,7 @@ void parseMultibootData(uint32 bootloaderMagic, uint32 multibootPhysLocation) {
 				displayData.pitch = framebufferTag->common.framebuffer_pitch;
 				displayData.depth = framebufferTag->common.framebuffer_bpp / 8;
 				// We wait to page in the framebuffer until it's needed and when advanced paging is available
-				// printf("Framebuffer: %d x %d, depth: %d, pitch: %d\n", displayData.width, displayData.height, displayData.depth, displayData.pitch);
+				DEBUG(printf("Framebuffer: %d x %d, depth: %d, pitch: %d\n", displayData.width, displayData.height, displayData.depth, displayData.pitch););
 				break;
 
 			case MULTIBOOT_TAG_TYPE_MMAP:
@@ -82,7 +82,7 @@ void parseMultibootData(uint32 bootloaderMagic, uint32 multibootPhysLocation) {
 				memMap.entryCount = (mmapTag->size - sizeof(struct multiboot_tag_mmap)) / mmapTag->entry_size;
 				memMap.entrySize = mmapTag->entry_size;
 				memMap.entries = (memoryMapEntry_t *)mmapTag->entries;
-				// printf("Memory map: version %d, %d entries, each entry is %d bytes\n", mmapTag->entry_version, memMap.entryCount, mmapTag->entry_size);
+				DEBUG(printf("Memory map: version %d, %d entries, each entry is %d bytes\n", mmapTag->entry_version, memMap.entryCount, mmapTag->entry_size););
 				break;
 
 			default: // Ignore all other tags
@@ -106,6 +106,8 @@ displayData_t *getDisplayData() {
 		displayData.framebufferVirtAddress
 			= mapPhysicalToKernel((void *)(uintptr_t)displayData.framebufferPhysAddress, displayData.framebufferSize, FLAG_PAGE_PRESENT | FLAG_PAGE_WRITABLE | FLAG_PAGE_WRITETHROUGH_CACHE);
 		isFramebufferMapped = true;
+	} else {
+		displayData.framebufferVirtAddress = (void *)0xB8000;
 	}
 	return &displayData;
 }

src/kernel/formatting.c
@@ -34,6 +34,10 @@ const char *numBytesToUnit(uint64 numBytes) {
 	}
 }
 
+const char *boolToString(bool b) {
+	return b ? "true" : "false";
+}
+
 void repeatString(const char *string, uint32 nTimes) {
 	for (uint32 i = 0; i < nTimes; i++) {
 		printf("%s", string);

src/kernel/formatting.h
@@ -5,6 +5,8 @@
 uint64 numBytesToHuman(uint64 numBytes);
 const char *numBytesToUnit(uint64 numBytes);
 
+const char *boolToString(bool b);
+
 void repeatString(const char *string, uint32 nTimes);
 
 uint32 getNumberLength(uint32 num, uint32 base);

src/kernel/interrupts/idt.c
@@ -15,14 +15,14 @@ typedef struct interruptDescriptor {
 	uint32 reserved;
 } __attribute__((packed)) interruptDescriptor;
 
-C_ASSERT(sizeof(interruptDescriptor) == 16);
+COMPILE_TIME_ASSERT(sizeof(interruptDescriptor) == 16, IDT_ENTRY_MUST_BE_16_BYTES);
 
 typedef struct idt_ptr {
 	uint16 size;
 	uint64 address;
 } __attribute__((packed)) idt_ptr;
 
-C_ASSERT(sizeof(idt_ptr) == 10);
+COMPILE_TIME_ASSERT(sizeof(idt_ptr) == 10, IDT_POINTER_STRUCT_MUST_BE_10_BYTES);
 
 __attribute__((aligned(0x10))) static interruptDescriptor idt[256] = {0};
 idt_ptr idt_pointer = {0};

src/kernel/interrupts/irq.c
@@ -1,4 +1,6 @@
 #include <interrupts/idt.h>
+#include <interrupts/isrs.h>
+#include <interrupts/pic.h>
 #include <io.h>
 #include <stdio.h>
 #include <system.h>
@@ -23,26 +25,27 @@ extern void irq15();
 void *irq_routines[16] = {0};
 
 void irq_install_handler(int irq, void (*handler)(struct regs *r)) {
+	assert(irq >= 0 && irq < 16, "Invalid IRQ number");
 	irq_routines[irq] = handler;
 }
 
 void initIRQs() {
-	idt_set_gate(32, (uintptr_t)irq0, GATE_INTERRUPT, 0);
-	idt_set_gate(33, (uintptr_t)irq1, GATE_INTERRUPT, 0);
-	idt_set_gate(34, (uintptr_t)irq2, GATE_INTERRUPT, 0);
-	idt_set_gate(35, (uintptr_t)irq3, GATE_INTERRUPT, 0);
-	idt_set_gate(36, (uintptr_t)irq4, GATE_INTERRUPT, 0);
-	idt_set_gate(37, (uintptr_t)irq5, GATE_INTERRUPT, 0);
-	idt_set_gate(38, (uintptr_t)irq6, GATE_INTERRUPT, 0);
-	idt_set_gate(39, (uintptr_t)irq7, GATE_INTERRUPT, 0);
-	idt_set_gate(40, (uintptr_t)irq8, GATE_INTERRUPT, 0);
-	idt_set_gate(41, (uintptr_t)irq9, GATE_INTERRUPT, 0);
-	idt_set_gate(42, (uintptr_t)irq10, GATE_INTERRUPT, 0);
-	idt_set_gate(43, (uintptr_t)irq11, GATE_INTERRUPT, 0);
-	idt_set_gate(44, (uintptr_t)irq12, GATE_INTERRUPT, 0);
-	idt_set_gate(45, (uintptr_t)irq13, GATE_INTERRUPT, 0);
-	idt_set_gate(46, (uintptr_t)irq14, GATE_INTERRUPT, 0);
-	idt_set_gate(47, (uintptr_t)irq15, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 0, (uintptr_t)irq0, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 1, (uintptr_t)irq1, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 2, (uintptr_t)irq2, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 3, (uintptr_t)irq3, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 4, (uintptr_t)irq4, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 5, (uintptr_t)irq5, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 6, (uintptr_t)irq6, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 7, (uintptr_t)irq7, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 8, (uintptr_t)irq8, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 9, (uintptr_t)irq9, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 10, (uintptr_t)irq10, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 11, (uintptr_t)irq11, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 12, (uintptr_t)irq12, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 13, (uintptr_t)irq13, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 14, (uintptr_t)irq14, GATE_INTERRUPT, 0);
+	idt_set_gate(32 + 15, (uintptr_t)irq15, GATE_INTERRUPT, 0);
 }
 
 void irq_handler(struct regs *r) {
@@ -55,12 +58,9 @@ void irq_handler(struct regs *r) {
 		printf("\n [!!!] Unhandled IRQ: %lu\n", r->int_no - 32);
 	}
 
-
 	// send an EOI (end of interrupt) command to the PICs.
-	if (r->int_no >= 40)
-	{
-		outb(0xA0, 0x20);
+	if (r->int_no - 32 >= 8) {
+		outb(PIC2_COMMAND_PORT, PIC_COMMAND_EOI);
 	}
-	outb(0x20, 0x20);
+	outb(PIC1_COMMAND_PORT, PIC_COMMAND_EOI);
 }
-	

src/kernel/interrupts/irq.h
@@ -1,3 +1,4 @@
+#include <interrupts/isrs.h>
 #include <system.h>
 
 #pragma once

src/kernel/interrupts/isrs.c
@@ -160,7 +160,7 @@ void fault_handler(struct regs *r) {
 				default:
 					// GCC doesn't realize that the switch statement covers all possible values and insists
 					// we either do this or disable the relevant warning for this section.
-					assertf("This line should be unreachable!");
+					panic("This line should be unreachable!");
 					faultTable = "unknown descriptor table";
 					break;
 			}

src/kernel/interrupts/isrs.h
@@ -4,23 +4,31 @@
 
 void initISRs();
 
+// Contents of the stack after an interrupt
+struct regs {
+	uint64 rax, rbx, rcx, rdx, rsi, rdi, rbp, r8, r9, r10, r11, r12, r13, r14, r15;
 
-#define E_DIVISION_BY_ZERO 0
-#define E_DEBUG 1
-#define E_NON_MASKABLE_INTERRUPT 2
-#define E_BREAKPOINT 3
-#define E_INTO_DETECTED_OVERFLOW 4
-#define E_OUT_OF_BOUNDS 5
-#define E_INVALID_OPCODE 6
-#define E_NO_COPROCESSOR 7
-#define E_DOUBLE_FAULT 8
+	uint64 int_no, err_code;
+
+	uint64 rip, cs, rflags, rsp, ss;
+};
+
+#define E_DIVISION_BY_ZERO            0
+#define E_DEBUG                       1
+#define E_NON_MASKABLE_INTERRUPT      2
+#define E_BREAKPOINT                  3
+#define E_INTO_DETECTED_OVERFLOW      4
+#define E_OUT_OF_BOUNDS               5
+#define E_INVALID_OPCODE              6
+#define E_NO_COPROCESSOR              7
+#define E_DOUBLE_FAULT                8
 #define E_COPROCESSOR_SEGMENT_OVERRUN 9
-#define E_BAD_TSS 10
-#define E_SEGMENT_NOT_PRESENT 11
-#define E_STACK_FAULT 12
-#define E_GENERAL_PROTECTION_FAULT 13
-#define E_PAGE_FAULT 14
-#define E_UNKNOWN_INTERRUPT 15
-#define E_COPROCESSOR_FAULT 16
-#define E_ALIGNMENT_CHECK 17
-#define E_MACHINE_CHECK 18
+#define E_BAD_TSS                     10
+#define E_SEGMENT_NOT_PRESENT         11
+#define E_STACK_FAULT                 12
+#define E_GENERAL_PROTECTION_FAULT    13
+#define E_PAGE_FAULT                  14
+#define E_UNKNOWN_INTERRUPT           15
+#define E_COPROCESSOR_FAULT           16
+#define E_ALIGNMENT_CHECK             17
+#define E_MACHINE_CHECK               18

commit e3b68cec7f23f0381265c1255623f4bd5851efbf

Undone error introduced in aa0844d07d0298eab3d774b54361b933625120a9

src/kernel/multibootdata.c
@@ -91,6 +91,13 @@ void parseMultibootData(uint32 bootloaderMagic, uint32 multibootPhysLocation) {
 
 		tag = (void *)tag + ((tag->size + 7) & ~7ul);
 	}
+
+	if (!displayData.isGraphicalFramebuffer) {
+		displayData.framebufferPhysAddress = 0xB8000;
+		displayData.framebufferSize = 80 * 25 * 2;
+		displayData.width = 80;
+		displayData.height = 25;
+	}
 }
 
 bootData_t *getBootData() {
@@ -106,8 +113,6 @@ displayData_t *getDisplayData() {
 		displayData.framebufferVirtAddress
 			= mapPhysicalToKernel((void *)(uintptr_t)displayData.framebufferPhysAddress, displayData.framebufferSize, FLAG_PAGE_PRESENT | FLAG_PAGE_WRITABLE | FLAG_PAGE_WRITETHROUGH_CACHE);
 		isFramebufferMapped = true;
-	} else {
-		displayData.framebufferVirtAddress = (void *)0xB8000;
 	}
 	return &displayData;
 }